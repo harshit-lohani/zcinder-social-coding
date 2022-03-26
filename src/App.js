9304e0e5191316c492e681c8a6c5235ffc69c5ce
Omniwot <52799841+Omniwot@users.noreply.github.com>
2022-03-26 23:14:29 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import {Routes, Route} from 'react-router-dom'
 import Login from './Login';
 import Dashboard from './Dashboard';
 
+
 function App() {
   return (
     <div className="App">
+       
     <Routes>
+    
+       
+      
       <Route exact path='/' element={<Login/>}/>
       <Route path = '/dashboard' element={<Dashboard/>}/>
     </Routes>
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import DashNav from './components/DashNav'
+import Sidebar from './components/NavSidebar'
+import './dashboard.css'
 
 function Dashboard() {
     
     
     return (
         <div>
-    <div>Dashboard</div>
-    <DashNav/>
-    </div>
+            <div>Dashboard</div>
+            <div className='main'>
+                
+                <div className='dashnav'>
+                    <DashNav/>
+                 </div>
+            </div>
+    
+        </div>
   )
 }
 
