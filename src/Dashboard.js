308df11fc203388f33ffa67138098cc166aa092b
Omniwot <52799841+Omniwot@users.noreply.github.com>
2022-03-26 17:41:23 +0530

    working version without nav and api

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,26 +1,8 @@
 import React from 'react'
 import {Link} from "react-router-dom"
-import Codeforces from '../node_modules/codeforces-api';
 
 function Dashboard() {
-    // var env = require('env');
     
-/*Codeforces.setApis('c355b982df9c86902c506e41e7a5ffa7cd432792', '4af7cda608b19e67c230f59c8be0f6a5df6138db');
-
-var parameters = {
-    handle: 'ishangarg09',
-    // onlyOnline: false
-};
-
-Codeforces.user.status(parameters, function (err,data) {
-    if(err){
-        return console.log(err);
-    }
-    console.log(data);
-    // const obj = JSON.parse(data);
-    // console.log(obj.id);
-});*/
-
     
     return (
     <div>Dashboard</div>
