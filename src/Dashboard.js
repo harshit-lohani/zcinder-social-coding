e09940e22c35752330af5f2c59174623b34dab68
ishangarg9 <67002620+ishangarg9@users.noreply.github.com>
2022-03-26 19:04:30 +0530

    leetcode and interviewbit done

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import leetcode from './leetcode'
+import interviewbit from './interviewbit'
+
+// var Codeforces = require('./node_modules/codeforces-api');
+// const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+// async function load() {
+  
+// }
+
+
 
 function Dashboard() {
-    
-    
+  let obj = interviewbit('ishangarg');
+
+  console.log(obj);
+  // leetcode('ishangarg');
+
     return (
     <div>Dashboard</div>
   )
 }
+
+
 
 export default Dashboard
