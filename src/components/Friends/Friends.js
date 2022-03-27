0cda6a8a1f85ab1bb38df343d51d8319518401ea
Omniwot <52799841+Omniwot@users.noreply.github.com>
2022-03-28 04:39:12 +0530

    friends firebase

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,16 +1,55 @@
-import React from 'react'
 import Friend from './Friend'
 import "./Friends.css"
+import React, { useState,useEffect } from 'react'
+import {auth,provider, db} from "../../Firebase"
+import {doc,getDoc,updateDoc} from "firebase/firestore"
 
 function Friends() {
+  const [friendsData,setFriendsData] = useState([]) ;
+  const [friendList,setFriendList] = useState([]) ; 
+  
+  
+
+  const getFriendsList=async event=>{
+    var details = JSON.parse(localStorage.getItem('userDetails'));
+    const DocRef = doc(db, "users", `${details.profile.id}`);
+    getDoc(DocRef).then(response=>{
+      console.log(response._document.data.value.mapValue.fields.friends.arrayValue.values);
+      setFriendList(response._document.data.value.mapValue.fields.friends.arrayValue.values);
+    
+    friendList.map((friend, i) =>{
+      console.log(friend.stringValue);
+      const friendRef = doc(db, "users", `${friend.stringValue}`);
+     getDoc(friendRef)
+      .then(response =>{setFriendsData([...friendsData,response._document.data.value.mapValue.fields]);
+     console.log(response)});
+      
+    });
+  });
+  }
+
+  
+  
+  
+ useEffect(() => {
+    getFriendsList();
+  },[]);
+
+  const displayFriends=friendsData
+   .map((friend_dat) => {
+    return (
+      <Friend props={friend_dat}/>
+      );
+    })   ;
+  
+
   return (
     <div className='friend-container'>
         <div className='friend-header'>
-          Friends (415)
+          Friends ({friendsData.length})
         </div>
         <div className="friend-list">
-          <Friend />
-          <Friend />
+          {displayFriends}
         </div>
     </div>
   )
