89b04d924e6b6456711593a0898463f8fd54de41
ishangarg9 <67002620+ishangarg9@users.noreply.github.com>
2022-03-28 02:34:35 +0530

    githubGET

--- src/API-Get/githubGET.js
+++ src/API-Get/githubGET.js
@@ -1,24 +1,100 @@
 import github from "../github";
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 var gh_handle = 'ishangarg9';
 
-function githubGET(){
+var repos;
 
-    let obj = github(gh_handle);
-    var promise = Promise.resolve(obj);
-
-  promise.then(function(val) {
-    // contestName.push(val.contests['1']['Contest']);
-    console.log(val);
-    // console.log(val.contests['1']['Contest']);
-
-    document.getElementById('gh_repo').textContent = val['0']['clone_url'];
+async function repo_lang(url){
+  let obj = null;
     
+    console.log(url);
     
-
-
-
-});
+    try {
+      obj = await (await fetch(url)).json();
+    } catch(e) {
+        console.log('error');
+    }
+    
+  return obj;
 }
 
-export default githubGET;
+function githubGET(){
+
+    //getting user info
+
+    let obj1 = github(gh_handle, '');
+
+    var promise1 = Promise.resolve(obj1);
+
+    promise1.then(function(val1) {
+
+      console.log(val1);
+        document.getElementById('git-followers').textContent = val1.followers;
+        document.getElementById('git-following').textContent = val1.following;
+        document.getElementById('git-repos').textContent = val1.public_repos;
+        
+  
+      });
+
+    //getting repo and their tech stacks
+
+    
+
+}
+
+function githubREPO(gh_handle, repo_no){
+  let obj2 = github(gh_handle, '/repos');
+  var promise2 = Promise.resolve(obj2);
+
+  
+
+    promise2.then(function(val2) {
+
+      console.log(val2);
+
+      var repos = 0;
+
+      for(var r of val2){
+        repos++;
+      }
+      
+      console.log(repos);
+  
+      if(repo_no < repos){
+        //this repo exits
+        let vv = "" + repo_no;
+        console.log(val2[vv]['name']);
+        
+
+        let repo_url = val2[vv]['url'];
+
+        let lango = repo_lang(repo_url);
+
+        // console.log(lango);
+
+        var promise3 = Promise.resolve(lango);
+
+        promise3.then(function(val3) {
+
+          console.log(val3);
+
+          console.log(val3.language);
+
+          document.getElementById('repo-lang').textContent = val3.language;
+      
+          });
+
+          document.getElementById('git-repo-name').textContent = val2[vv]['name'];
+
+
+      }
+      else {
+        console.log("repo does not exists");
+      }
+  
+      });
+
+}
+
+export {githubGET, githubREPO};
