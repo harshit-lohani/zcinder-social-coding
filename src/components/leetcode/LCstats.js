f59660849ae0bebf9468d5a996f4a4e1c51803ee
ishangarg9 <67002620+ishangarg9@users.noreply.github.com>
2022-03-28 08:33:25 +0530

    leetcode added

---
+++ src/components/leetcode/LCstats.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import leetcodeGET from '../../API-Get/leetcodeGET';
+
+function LCstats(){
+    leetcodeGET('ishangarg09')
+    return (
+        <React.Fragment>
+            <div>Rank: <span id='lc-rank'></span></div>
+            <div>Acceptance Rate: <span id='lc-acc-rate'></span></div>
+            <div>Reputation: <span id='lc-reputation'></span></div>
+            <div>Easy Submitted: <span id='lc-easy'></span></div>
+            <div>Medium Submitted: <span id='lc-medium'></span></div>
+            <div>Hard Submitted: <span id='lc-hard'></span></div>
+            <div>Easy Acceptance Rate: <span id='lc-easy-acc'></span></div>
+            <div>Medium Acceptance Rate: <span id='lc-Medium-acc'></span></div>
+            <div>Hard Acceptance Rate: <span id='lc-Hard-acc'></span></div>
+
+        </React.Fragment>
+
+
+    )
+
+}
+
+export default LCstats;
